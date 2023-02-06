02758f6cbace14bcf0077691316057055898a6a5
HanseongBae <bha4388@naver.com>
2023-02-06 21:50:22 +0900

    feat : 스프링 시큐리티  URL 인가권한 설정.

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 리턴하겠다!!
 public class IndexController {
@@ -13,5 +14,43 @@ public class IndexController {
 		// 스프링에서 지원해주기 때문에 따로 설정하지 않아도 된다. 생략가능!!!
 		return "index"; //src/main/resources/templates/index.mustache
 	}
+	
+	@GetMapping("/user")
+	@ResponseBody
+	public String user() {
+		return "user";
+	}
+	
+	@GetMapping("/admin")
+	@ResponseBody
+	public String admin() {
+		return "admin";
+	}
+	
+	@GetMapping("/manager")
+	@ResponseBody
+	public String manager() {
+		return "manager";
+	}
+	
+	// 스프링 시큐리티 해당주소를 낚아챈다. - SecurityConfig 파일 생성후 작동이 안한다.
+	@GetMapping("/login")
+	@ResponseBody
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	@ResponseBody
+	public String join() {
+		return "join";
+	}
+	
+	@GetMapping("/joinProc")
+	@ResponseBody
+	public String joinProc() {
+		return "회원가입 완료됨";
+	}
+	
 	
 }
