844399692ef75b0218d49391e6e9a20912e9363d
HanseongBae <bha4388@naver.com>
2023-02-07 19:51:37 +0900

    feat : BCryptPasswordEncoder를 이용한 암호인코딩 및 회원가입 구현

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,24 @@
 package com.cos.security1.controller;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
 
 @Controller // View를 리턴하겠다!!
 public class IndexController {
+	
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public IndexController(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userRepository = userRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
 
 	@GetMapping({"","/"})
 	public String index() {
@@ -34,23 +47,31 @@ public class IndexController {
 	}
 	
 	// 스프링 시큐리티 해당주소를 낚아챈다. - SecurityConfig 파일 생성후 작동이 안한다.
-	@GetMapping("/login")
-	@ResponseBody
-	public String login() {
-		return "login";
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
 	}
 	
-	@GetMapping("/join")
-	@ResponseBody
-	public String join() {
-		return "join";
+	@PostMapping("/join")
+	public String join(User user) {
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		
+		user.setRole("ROLE_USER");
+		user.setPassword(encPassword);
+		
+		userRepository.save(user); 
+		// 회원가입은 잘됨. 비밀번호 노출됨. 
+		// 시큐리티로 로그인을 할수가없다. 이유는 패스워드가 암호화가 안되었기 때문에
+		// 인코딩을해야 보안 + 로그인이 가능하다.
+		return "redirect:/loginForm";
 	}
 	
-	@GetMapping("/joinProc")
-	@ResponseBody
-	public String joinProc() {
-		return "회원가입 완료됨";
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
 	}
+	
 	
 	
 }
